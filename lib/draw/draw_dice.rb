4a8405e19e7794eb6732051c7d464c5227a2b665
richardtemple <richardjamestemple@gmail.com>
2017-07-21 16:15:54 -0700

    add better images and background color

--- lib/draw/draw_dice.rb
+++ lib/draw/draw_dice.rb
@@ -5,19 +5,29 @@ include Gosu
 class DrawDice < Gosu::Window
 
   def initialize
-    super(800, 600)
+    super(1800, 1200)
     self.caption = "Jump 'n Run"
-    @one, @two, @three, @four = *Image.load_tiles(self, "./media/dice.png", 480, 490, true)
+    
+    @one   = Gosu::Image.new("./media/1.png")
+    @two   = Gosu::Image.new("./media/2.png")
+    @three = Gosu::Image.new("./media/3.png")
+    @four  = Gosu::Image.new("./media/4.png")
+    @five  = Gosu::Image.new("./media/5.png")
+    @six   = Gosu::Image.new("./media/6.png")
+
   end
 
   def update
   end
 
   def draw
+draw_rect(0, 0, 1800, 1200, Gosu::Color.argb(0xff_ffffff))
     scale = 0.2
     @one.draw(1, 1, 1, scale, scale )
     @two.draw(100, 1, 1, scale, scale )
     @three.draw(200, 1, 1, scale, scale )
     @four.draw(300, 1, 1, scale, scale )
+    @five.draw(400, 1, 1, scale, scale )
+    @six.draw(500, 1, 1, scale, scale )
   end
 end
