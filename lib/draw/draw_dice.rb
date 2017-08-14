fb0f7d605b38d0607a3fea87aeffe6686c28687a
richardtemple <richardjamestemple@gmail.com>
2017-08-14 11:30:51 -0700

    make mouse on top and window smaller and improve performance

--- lib/draw/draw_dice.rb
+++ lib/draw/draw_dice.rb
@@ -12,28 +12,32 @@ class DrawDice < Gosu::Window
     @m = Main.new
     @text   = Gosu::Font.new(20)
     @cursor = Gosu::Image.new("./media/MousePntr.png")
-    @message = "NO"
+    @current_player_msg = Gosu::Font.new(20)
+    @roll_button_label = Gosu::Font.new(20)
+    @end_button_label = Gosu::Font.new(20)
+    @player1_label = Gosu::Font.new(20)
+    @player2_label = Gosu::Font.new(20)
+
   end
 
   def update
   end
 
   def draw
-    draw_rect(0, 0, 1800, 1200, Gosu::Color.argb(0xff_ffffff))
+    draw_rect(0, 0, 1000, 500, Gosu::Color.argb(0xff_ffffff))
     # @text.draw("Score: HI Die = #{@message}", 10, 150, 1, 1.0, 1.0, Gosu::Color::BLACK)
-    @cursor.draw self.mouse_x, self.mouse_y, 0, 0.1, 0.1
+    @cursor.draw self.mouse_x, self.mouse_y, 10, 0.1, 0.1
     draw_rect(600, 1, 100, 100, Gosu::Color.argb(0xff_000000), z = 0, mode = :default)
-    Gosu::Font.new(20).draw("ROLL!", 615, 40, 1, 1.0, 1.0, Gosu::Color::WHITE)
+    @roll_button_label.draw("ROLL!", 615, 40, 1, 1.0, 1.0, Gosu::Color::WHITE)
     draw_rect(600, 200, 100, 100, Gosu::Color.argb(0xff_000000), z = 0, mode = :default)
-    Gosu::Font.new(20).draw("END!", 615, 240, 1, 1.0, 1.0, Gosu::Color::WHITE)
-    Gosu::Font.new(20).draw("Player #{@m.current_player} current hand = #{@m.current_hand_score}", 10, 150, 1, 1.0, 1.0, Gosu::Color::BLACK)
-    Gosu::Font.new(20).draw("Player 1 Score: #{@m.player_one_score}", 10, 200, 1, 1.0, 1.0, Gosu::Color::BLACK)
-    Gosu::Font.new(20).draw("Player 2 Score: #{@m.player_two_score}", 10, 250, 1, 1.0, 1.0, Gosu::Color::BLACK)
+    @end_button_label.draw("END!", 615, 240, 1, 1.0, 1.0, Gosu::Color::WHITE)
+    @current_player_msg.draw("Player #{@m.current_player} current hand = #{@m.current_hand_score}", 10, 150, 1, 1.0, 1.0, Gosu::Color::BLACK)
+    @player1_label.draw("Player 1 Score: #{@m.player_one_score}", 10, 200, 1, 1.0, 1.0, Gosu::Color::BLACK)
+    @player2_label.draw("Player 2 Score: #{@m.player_two_score}", 10, 250, 1, 1.0, 1.0, Gosu::Color::BLACK)
     @m.draw
   end
 
   def button_down(id)
-    @message = "YES"
     if mouse_y < 150
       case mouse_x
       when (0..100)
